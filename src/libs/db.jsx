2ec1cb5b19fb20f66498e39a6c8fab9a2569dce6
Nikolaus satria <nikolaussatria@gmail.com>
2024-08-16 17:18:00 +0700

    ADD PORT

--- src/libs/db.jsx
+++ src/libs/db.jsx
@@ -5,6 +5,7 @@ const pool = mysql.createPool({
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DB,
+  port: process.env.MYSQL_PORT,
   connectionLimit: 50,
 });
 
