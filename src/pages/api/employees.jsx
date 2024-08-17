212c2fe24e6334be68963ce1143ed49048ad0574
Nikolaus satria <nikolaussatria@gmail.com>
2024-08-17 12:48:22 +0700

    employees api

---
+++ src/pages/api/employees.jsx
@@ -0,0 +1,18 @@
+import { query } from "@/libs/db";
+
+export default async function handler(req, res) {
+  if (req.method !== "GET") {
+    return res.status(405).json({ message: 'Method Not Allowed' });
+  }
+
+  try {
+    const employees = await query({
+      query: 'SELECT id, name, position FROM employees',
+    });
+
+    return res.status(200).json({ employees });
+  } catch (error) {
+    console.error("Error fetching employees:", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
