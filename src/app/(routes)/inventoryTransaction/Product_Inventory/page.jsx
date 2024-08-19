db6edd10726db9cc2b54eecd3b8e8302f74e0f2b
Nikolaus satria <nikolaussatria@gmail.com>
2024-08-19 13:48:44 +0700

    product-inventory fix

--- src/app/(routes)/inventoryTransaction/Product_Inventory/page.jsx
+++ src/app/(routes)/inventoryTransaction/Product_Inventory/page.jsx
@@ -208,7 +208,8 @@ function ProductInventoryTransaction() {
                 <th className="px-7 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
                   Quantity
                 </th>
-                <th className="px-6 py-3 border-b-2 border-gray-300"></th>
+                <th className="px-6 py-3 border-b-2 border-gray-300 text-blue-500">Action
+                </th>
               </tr>
             </thead>
 
