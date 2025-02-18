8c93840eb1a36358f7e4475b0eb6a546ebfd9b8b
Nikolaus Satria <nikolaussatria@gmail.com>
2025-02-18 11:26:52 +0700

    feat: improve layout and styling of Material page components

--- src/app/(routes)/databaseSKU/page.jsx
+++ src/app/(routes)/databaseSKU/page.jsx
@@ -53,48 +53,26 @@ function DatabaseSku() {
 
   return (
     <RouteLayout>
-      <div className="flex h-full p-5 flex-col bg-white text-left font-sans font-medium shadow-md">
-        {/* Header Button */}
-        <div className="flex justify-between items-center">
-          <h1 className="font-medium text-3xl flex-1 text-center">
+      <div className="flex flex-col h-full p-5 bg-white font-sans text-left shadow-md">
+        {/* Header */}
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-3xl font-medium text-center flex-1">
             DATABASE SKU
           </h1>
           <Link href={"databaseSKU/Product/ProductForm"}>
-            <button>
-              <BsDatabaseAdd size={"30px"} className="cursor-pointer" />
+            <button className="cursor-pointer">
+              <BsDatabaseAdd size={"30px"} />
             </button>
           </Link>
         </div>
-        {/* Search Bar container */}
-        <form className="p-7" onSubmit={handleSearch}>
-          <label
-            htmlFor="default-search"
-            className="mb-2 text-sm font-medium text-gray-900 sr-only"
-          >
-            Search
-          </label>
+
+        {/* Search Bar */}
+        <form className="mb-6" onSubmit={handleSearch}>
           <div className="relative">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                aria-hidden="true"
-                className="w-5 h-5 text-gray-500"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                ></path>
-              </svg>
-            </div>
             <input
               type="search"
               id="default-search"
-              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-700 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500"
+              className="block w-full p-4 pl-10 text-sm border border-gray-700 rounded-lg bg-white focus:ring-blue-500 focus:border-blue-500"
               placeholder="Search Inventory by Name or ID"
               value={searchQuery}
               onChange={handleSearchChange}
@@ -102,97 +80,95 @@ function DatabaseSku() {
             />
             <button
               type="submit"
-              className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2"
+              className="absolute right-2.5 bottom-2.5 px-4 py-2 text-sm font-medium text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
             >
               Search
             </button>
           </div>
         </form>
-        {/* Categories Menubar */}
-        <div className="flex flex-row-reverse">
-          <nav className="relative z-0 inline-flex shadow-sm">
-            <div className="flex justify-center items-center">
-              <Link
-                href="/databaseSKU/Product"
-                className="-ml-px relative inline-flex items-center px-4 py-2 border rounded border-gray-300 bg-white text-sm leading-5 font-medium text-blue-700 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-tertiary active:text-gray-700 transition ease-in-out duration-200 hover:bg-blue-700 hover:text-white"
-              >
-                Product
-              </Link>
-              <Link
-                href="/databaseSKU/Asset"
-                className="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-blue-600 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-tertiary active:text-gray-700 transition ease-in-out duration-200 hover:bg-blue-700 hover:text-white"
-              >
-                Asset
-              </Link>
-              <Link
-                href="/databaseSKU/Material"
-                className="-ml-px relative inline-flex items-center px-4 py-2 border rounded border-gray-300 bg-white text-sm leading-5 font-medium text-blue-600 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-tertiary active:text-gray-700 transition ease-in-out duration-200 hover:bg-blue-700 hover:text-white"
-              >
-                Material
-              </Link>
-            </div>
-          </nav>
-        </div>
 
-        <div className="justify-center items-center max-w-full max-h-screen shadow bg-white shadow-dashboard px-4 pt-5 mt-4 rounded-bl-lg rounded-br-lg overflow-y-auto overflow-x">
-          <table className="min-w-full">
-            {/* Head table */}
+        {/* Categories Menu */}
+        <nav className="flex justify-center space-x-4 mb-6">
+          <Link
+            href="/databaseSKU/Product"
+            className="px-4 py-2 text-sm font-medium text-blue-700 border border-gray-300 rounded hover:bg-blue-700 hover:text-white transition duration-200"
+          >
+            Product
+          </Link>
+          <Link
+            href="/databaseSKU/Asset"
+            className="px-4 py-2 text-sm font-medium text-blue-700 border border-gray-300 rounded hover:bg-blue-700 hover:text-white transition duration-200"
+          >
+            Asset
+          </Link>
+          <Link
+            href="/databaseSKU/Material"
+            className="px-4 py-2 text-sm font-medium text-blue-700 border border-gray-300 rounded hover:bg-blue-700 hover:text-white transition duration-200"
+          >
+            Material
+          </Link>
+        </nav>
+
+        {/* Table */}
+        <div className="flex-grow overflow-y-auto shadow rounded-lg">
+          <table className="min-w-full bg-white">
+            {/* Table Head */}
             <thead>
-              <tr>
-                <th className="px-2 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
+              <tr className="border-b border-gray-300">
+                <th className="px-4 py-3 text-left text-sm font-medium text-blue-500">
                   No
                 </th>
-                <th className="px-2 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
+                <th className="px-4 py-3 text-left text-sm font-medium text-blue-500">
                   ID
                 </th>
-                <th className="px-4 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
+                <th className="px-4 py-3 text-left text-sm font-medium text-blue-500">
                   Product Description
                 </th>
-                <th className="px-4 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
+                <th className="px-4 py-3 text-left text-sm font-medium text-blue-500">
                   Type
                 </th>
-                <th className="px-4 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
+                <th className="px-4 py-3 text-left text-sm font-medium text-blue-500">
                   Material Type
                 </th>
-                <th className="px-4 py-3 border-b-2 border-gray-300 text-left text-sm leading-4 text-blue-500 tracking-wider">
+                <th className="px-4 py-3 text-left text-sm font-medium text-blue-500">
                   UOM
                 </th>
               </tr>
             </thead>
-
-            <tbody className="bg-white">
+            {/* Table Body */}
+            <tbody>
               {isLoading ? (
                 <tr>
-                  <td colSpan="5" className="text-center py-4">
+                  <td colSpan="6" className="text-center py-4">
                     Loading...
                   </td>
                 </tr>
               ) : Array.isArray(dbSku) && dbSku.length > 0 ? (
                 dbSku.map((item, index) => (
-                  <tr key={index}>
-                    <td className="px-2 py-4 whitespace-no-wrap border-b border-gray-300 text-sm leading-5 text-gray-500">
+                  <tr key={index} className="border-b border-gray-300">
+                    <td className="px-4 py-4 text-sm text-gray-500">
                       {(currentPage - 1) * itemsPerPage + index + 1}
                     </td>
-                    <td className="px-2 py-4 whitespace-no-wrap border-b border-gray-300 text-sm leading-5 text-gray-500">
+                    <td className="px-4 py-4 text-sm text-gray-500">
                       #{item.id}
                     </td>
-                    <td className="px-4 py-4 whitespace-no-wrap border-b border-gray-300 text-sm leading-5 text-gray-500">
+                    <td className="px-4 py-4 text-sm text-gray-500">
                       {item.description}
                     </td>
-                    <td className="px-4 py-4 whitespace-no-wrap border-b border-gray-300 text-sm leading-5 text-gray-500">
+                    <td className="px-4 py-4 text-sm text-gray-500">
                       {item.type}
                     </td>
-                    <td className="px-4 py-4 whitespace-no-wrap border-b border-gray-300 text-sm leading-5 text-gray-500">
+                    <td className="px-4 py-4 text-sm text-gray-500">
                       {item.material_type}
                     </td>
-                    <td className="px-4 py-4 whitespace-no-wrap border-b border-gray-300 text-sm leading-5 text-gray-500">
+                    <td className="px-4 py-4 text-sm text-gray-500">
                       {item.uom}
                     </td>
                   </tr>
                 ))
               ) : (
                 <tr>
-                  <td colSpan="11" className="text-center py-4">
+                  <td colSpan="6" className="text-center py-4">
                     No data available
                   </td>
                 </tr>
@@ -200,6 +176,7 @@ function DatabaseSku() {
             </tbody>
           </table>
         </div>
+
         {/* Pagination */}
         <div className="flex justify-between mt-4">
           <button
@@ -207,11 +184,11 @@ function DatabaseSku() {
               setCurrentPage((prevPage) => Math.max(prevPage - 1, 1))
             }
             disabled={currentPage === 1}
-            className="px-4 py-2 bg-blue-700 text-white rounded disabled:opacity-50"
+            className="px-4 py-2 text-sm font-medium text-white bg-blue-700 rounded disabled:opacity-50"
           >
             Previous
           </button>
-          <span>
+          <span className="text-sm text-gray-700">
             Page {currentPage} of {totalPages}
           </span>
           <button
@@ -219,7 +196,7 @@ function DatabaseSku() {
               setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages))
             }
             disabled={currentPage === totalPages}
-            className="px-4 py-2 bg-blue-700 text-white rounded disabled:opacity-50"
+            className="px-4 py-2 text-sm font-medium text-white bg-blue-700 rounded disabled:opacity-50"
           >
             Next
           </button>
